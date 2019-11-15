17a3d83db5407a32bb3d18dab471616c74c98ba8
Andrew Pratt <apratt957@gmail.com>
2019-11-15 11:50:27 -0600

    styling

--- popup.js
+++ popup.js
@@ -2,25 +2,28 @@ const addressSubmit = document.getElementById('addressSubmit');
 
 addressSubmit.addEventListener('click', async function() {
   const address = document.getElementById('address');
-  const location = address.value;
-  const town = location.split(',')[0];
-  const randomNum = Math.ceil(Math.random() * 50);
-  var lat, long, videos;
+  const location = address.value.replace(/\s/g, '');
+  const splitTown = location.split(',')[0];
+  const spacedTown = splitTown.replace(/([a-z])([A-Z])/g, '$1 $2');
+  const parsedTown = spacedTown.replace(/\s/g, '%20');
+  let lat, long, videos;
 
   await fetch(
-    `http://open.mapquestapi.com/geocoding/v1/address?key=BGEiXmF2CNgWjtJUKgb2Tbv6yQClh0EH&location=${location}`
+    `https://us1.locationiq.com/v1/search.php?key=683f12733c5177&q=${parsedTown}&format=json`
   )
     .then(response => response.json())
     .then(response => {
-      lat = response.results[0].locations[0].latLng.lat;
-      long = response.results[0].locations[0].latLng.lng;
+      lat = response[0].lat;
+      long = response[0].lon;
     });
 
   await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&location=${lat}%2C%20${long}&locationRadius=5km&maxResults=50&q=${town}&type=video&key=AIzaSyBi3F8awhualQdnsvRm7Y0A3sJN1LabhEY`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&location=${lat}%2C%20${long}&locationRadius=5km&maxResults=50&q=${parsedTown}&type=video&key=AIzaSyBi3F8awhualQdnsvRm7Y0A3sJN1LabhEY`
   )
     .then(response => response.json())
     .then(response => (videos = response.items));
+
+  const randomNum = Math.floor(Math.random() * videos.length);
 
   chrome.tabs.create({
     url: `https://www.youtube.com/watch?v=${videos[randomNum].id.videoId}`
